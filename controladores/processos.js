cfaed183a41e4b71ac353aa0b1589cc5d6a0d4f1
Sergio Cardoso <desendata@gmail.com>
2022-10-14 11:57:22 -0300

    processo 27

--- controladores/processos.js
+++ controladores/processos.js
@@ -129,11 +129,11 @@ module.exports.addProcesso_WebHooks = addProcesso_WebHooks;
 const updateProcesso_Phoenix = (request, response) => {
   //  const { id } = request.body
     
-    const id = parseInt(request.params.id)    
+//    const id = parseInt(request.params.id)    
 
     pool.query(
         'update Processos set Situacao = $1  where id = $2',
-        [ '1' , id],
+        [ 1 , id],
         (error) => {
             if (error) {
                 return response.status(401).json({ status: 'error', 
