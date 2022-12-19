b073b574cf884d3d5d0cf6202f255b88a5243cf5
Sergio Cardoso <desendata@gmail.com>
2022-12-19 13:36:06 -0300

    processo 41

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-//const isProduction = process.env.NODE_ENV === 'production'
-const isProduction =false
+const isProduction = process.env.NODE_ENV === 'production'
+//const isProduction =false
 
 const {Pool} = require('pg')
 
@@ -10,8 +10,8 @@ const {Pool} = require('pg')
 
 //const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`
 
-//const connectionString = 'postgres://frngxudyyfvaxb:9823002c34b57b4daca5d0245ef3bd4fce5dfd92bcd2ffeb430575944a785034@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d3im7u02qpgk60'
-const connectionString = 'postgres://qlpkhvlz:YN9u6TIS3U8q1SwaWdqwJo86x9uBM1kZ@kesavan.db.elephantsql.com/qlpkhvlz'
+const connectionString = 'postgres://frngxudyyfvaxb:9823002c34b57b4daca5d0245ef3bd4fce5dfd92bcd2ffeb430575944a785034@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d3im7u02qpgk60'
+//const connectionString = 'postgres://qlpkhvlz:YN9u6TIS3U8q1SwaWdqwJo86x9uBM1kZ@kesavan.db.elephantsql.com/qlpkhvlz'
 
 
 //const connectionString = `postgresql://postgres:adm234@localhost/phoenix_crm`
