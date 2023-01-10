83333ea466855411444d386de9ae6816a61e3ce7
Sergio Cardoso <desendata@gmail.com>
2023-01-10 12:41:00 -0300

    processo 46

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 //const isProduction = process.env.NODE_ENV === 'production'
-const isProduction =false // <- Para produção comentar essa linha
+ const isProduction =true // <- Para produção comentar essa linha
 
 const {Pool} = require('pg')
 
@@ -11,13 +11,10 @@ const {Pool} = require('pg')
 //const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`
 
 //const connectionString = 'postgres://frngxudyyfvaxb:9823002c34b57b4daca5d0245ef3bd4fce5dfd92bcd2ffeb430575944a785034@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d3im7u02qpgk60'
-const connectionString = 'postgres://qlpkhvlz:YN9u6TIS3U8q1SwaWdqwJo86x9uBM1kZ@kesavan.db.elephantsql.com/qlpkhvlz' // <- Para produção comentar essa linha
 
+//10/01/23const connectionString = 'postgres://qlpkhvlz:YN9u6TIS3U8q1SwaWdqwJo86x9uBM1kZ@kesavan.db.elephantsql.com/qlpkhvlz' // <- Para produção comentar essa linha
 
 //const connectionString = `postgresql://postgres:adm234@localhost/phoenix_crm`
-
-
-
 
 
 
