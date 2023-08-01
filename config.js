b7438571a554c535616e3bd2cbfdab9fc3d8f22e
Sergio Cardoso <desendata@gmail.com>
2023-08-01 12:45:22 -0300

    processo 70

--- config.js
+++ config.js
@@ -13,13 +13,14 @@ const {Pool} = require('pg')
 //const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 // Habilitar p/ Heroku
-//29/07/23 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`
+//29/07/23 const
+ connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`
 
 //const connectionString = 'postgres://frngxudyyfvaxb:9823002c34b57b4daca5d0245ef3bd4fce5dfd92bcd2ffeb430575944a785034@ec2-44-209-24-62.compute-1.amazonaws.com:5432/d3im7u02qpgk60'
 
 //10/01/23const connectionString = 'postgres://qlpkhvlz:YN9u6TIS3U8q1SwaWdqwJo86x9uBM1kZ@kesavan.db.elephantsql.com/qlpkhvlz' // <- Para produção comentar essa linha
 
-const connectionString = `postgresql://postgres:123@localhost:5432/phoenix`  //29/07/23
+// desabilitado em 01/08/23 const connectionString = `postgresql://postgres:123@localhost:5432/phoenix`  //29/07/23
 
 
 
