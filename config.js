2af2ae306fc721f0668c2173387c8786e8d75643
Sergio Cardoso <desendata@gmail.com>
2023-09-11 15:08:56 -0300

    processo 102

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ const {Pool} = require('pg')
 
 ///Apnas mudar Esses dois comandos e mais o SSL para mudar de ambiente ( Em 10/09 )
 ///apaguei em 10/09 
-//const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`   // Habilitada para o Heroku , desabilitada para Localhost
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}`   // Habilitada para o Heroku , desabilitada para Localhost
 /// Essa linha precisa para o LOCALHOST liberei linha abaixo em 10/09          
-const connectionString = `postgresql://postgres:123@localhost:5432/phoenix`                                                                 // Desabilitada para o Heroku , Habilitada para Localhost
+//const connectionString = `postgresql://postgres:123@localhost:5432/phoenix`                                                                 // Desabilitada para o Heroku , Habilitada para Localhost
 
 
 
@@ -47,9 +47,9 @@ const pool = new Pool({ //connectionString
     connectionString: isProduction ? process.env.DATABASE_URL: connectionString
     // descomentar para fazer deploy no heroku
     //20/08/23 comentei as 3 linhas abaixo - Para o Heroku, as 3 linhas abaixo dvem estar dscomentadas
-    //, ssl: {
-    //           rejectUnauthorized: false,
-    //       }
+    , ssl: {
+               rejectUnauthorized: false,
+           }
     })
 
 
